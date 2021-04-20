e612cd911ddf1b9dbbd3100e284f354dfc78c53a
Irakli Maisuradze <82453236+Burnyco@users.noreply.github.com>
2021-04-20 08:03:59 +0400

    JS

---
+++ main.js
@@ -0,0 +1,25 @@
+function clock() {
+    const fullDate = new Date();
+    var hours = fullDate.getHours();
+    var mins = fullDate.getMinutes();
+    var sec = fullDate.getSeconds();
+
+    if (hours < 10) {
+        hours = '0' + hours;
+    }
+
+    if (mins < 10) {
+        mins = '0' + mins;
+    }
+
+    if (sec < 10) {
+        sec = '0' + sec;
+    }
+
+    document.getElementById('hour').innerHTML = '' + hours;
+    document.getElementById('minute').innerHTML = '' + mins;
+    document.getElementById('second').innerHTML = '' + sec;
+
+
+}
+setInterval(clock, 100);
